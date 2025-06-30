7f26dc57e87841ca3d26e679017272d78f8e35a4
sage-etcher <sage.message@email.com>
2025-06-30 09:48:15 -0400

    add alternate posix.prog.mk script for sbin installs

---
+++ maketools/posix.sprog.mk
@@ -0,0 +1,42 @@
+
+include $(PROJECT_ROOT)/maketools/posix.macros.mk
+include makefile.depend
+
+MANNUM = `echo "$(MAN)" |rev |cut -c 1`
+FULL_MANDIR := $(MANDIR)/man$(MANNUM)
+
+build: $(PROG) $(MAN).gz
+
+clean:
+	rm -f $(PROG)
+	rm -f $(MAN).gz
+
+install: build
+	install -d -D -m 0755 $(SBINDIR)
+	install -m 0755 -t $(SBINDIR) $(PROG)
+	test -z "$(MAN)" || install -d -D -m 0755 $(FULL_MANDIR)
+	test -z "$(MAN)" || install -m 0644 -t $(FULL_MANDIR) $(MAN).gz
+
+uninstall:
+	rm -f $(SBINDIR)/$(PROG)
+	rm -f $(FULL_MANDIR)/$(MAN).gz
+
+depend: makefile.depend
+
+debug:
+	echo "$(FULL_MANDIR)"
+
+$(PROG): $(SRCS)
+	$(CC) -o $@ $(CFLAGS) $(LDFLAGS) $(SRCS)
+
+$(MAN).gz: $(MAN)
+	-gzip -k9 $(MAN)
+
+makefile.depend:
+	cc -M $(SRCS) $(CFLAGS) >$@
+
+
+.PHONY: build clean install uninstall depend debug
+
+# vim: noet
+# end of file
