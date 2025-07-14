e64bd7bdf7509818deaf30acaf88821c4f89863e
sage-etcher <sage.message@email.com>
2025-07-14 15:48:39 -0400

    add chgrp command

---
+++ sbin/chgrp/core/chgrp.h
@@ -0,0 +1,18 @@
+
+#ifndef CHGRP_H
+#define CHGRP_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <sys/types.h>
+
+
+int chgrp_main (int argc, char **argv);
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif
+/* end of file */
